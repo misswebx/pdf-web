60e3dc4b37322645be6cd8c6da46591cc43ea673
dev <dev@dev.com>
2026-03-31 06:06:19 +0800

    add

--- pdfcraft/src/config/site.ts
+++ pdfcraft/src/config/site.ts
@@ -4,11 +4,11 @@
 export const siteConfig = {
   name: 'PDFCraft',
   description: 'Professional PDF Tools - Free, Private & Browser-Based. Merge, split, compress, convert, and edit PDF files online without uploading to servers.',
-  url: 'https://pdfcraft.devtoolcafe.com',
+  url: 'https://pdf.xaygo.com',
   ogImage: '/images/og-image.png',
   links: {
-    github: 'https://github.com/PDFCraftTool/pdfcraft',
-    twitter: 'https://twitter.com/pdfcraft',
+    github: 'https://github.com/misswebx/pdf-web',
+    twitter: '',
   },
   creator: 'PDFCraft Team',
   keywords: [
